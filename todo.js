53b9286bd4ac478ee7cb545ae33ddb3d6d9c9243
MaSsoumeh <massoumehzarei@gmail.com>
2021-09-17 03:48:34 +0430

    modify clearCompleted function/ extract some code to function /Change some variable and function names/commenting

--- todo.js
+++ todo.js
@@ -6,12 +6,12 @@ inputBox.addEventListener("keypress", keyPressCheck);
 const divTask = document.getElementById("task-div-id");
 let isDark = false;
 
-function createElement() {
+function createTodo() {
   const myInput = inputBox.value;
   if (myInput === "") {
     inputBox.placeholder = "Please Enter a task to do...";
   } else {
-    // Create New Elements
+    // Create Elements
     const divElem = document.createElement("div");
     const divEachItem = document.createElement("div");
     const imgCircle = new Image(20, 20);
@@ -22,37 +22,39 @@ function createElement() {
     const newContent = document.createTextNode(myInput);
     const lineBreak = document.createElement("hr");
 
-    // Set class
-    divElem.classList.add("elements");
-    divElem.classList.add("all");
-    divEachItem.classList.add("each-item");
-    imgCircle.classList.add("circle");
-    imgCross.classList.add("cross");
-    lineBreak.classList.add("horizontalLine");
-    lineBreak.classList.add("all");
+    // Call setClass()
+    setClass(divElem, divEachItem, imgCircle, imgCross, lineBreak);
 
-    //Append Child
-    divTask.appendChild(divElem);
-    divTask.appendChild(lineBreak);
-    divElem.appendChild(divEachItem);
-    divElem.appendChild(imgCross);
-    divEachItem.appendChild(imgCircle);
-    divEachItem.appendChild(spanContent);
-    spanContent.appendChild(newContent);
+    //Call createChild()
+    createChild(
+      divElem,
+      lineBreak,
+      divEachItem,
+      imgCross,
+      imgCircle,
+      spanContent,
+      newContent
+    );
+
     inputBox.value = "";
     inputBox.placeholder = "Create a new todo";
     let isCompleted = false;
-    activeLen();
-    //Delete Task Function
+    calcLeftItems();
+
+    //listener on delete-icon
     imgCross.addEventListener("click", deleteTask);
+
+    //listener on circle and Text
+    imgCircle.addEventListener("click", completeTask);
+    spanContent.addEventListener("click", completeTask);
+
+    //Delete Task
     function deleteTask() {
       divTask.removeChild(divElem);
       divTask.removeChild(lineBreak);
-      activeLen();
+      calcLeftItems();
     }
-    //Completed Task Function
-    imgCircle.addEventListener("click", completeTask);
-    spanContent.addEventListener("click", completeTask);
+    //Complete Task
     function completeTask() {
       divElem.classList.toggle("completed");
       lineBreak.classList.toggle("completed");
@@ -60,67 +62,91 @@ function createElement() {
       imgCircle.src = isCompleted
         ? "./images/check.svg"
         : "./images/circle.svg";
-      activeLen();
+      calcLeftItems();
+    }
+    //Append Child
+    function createChild(
+      divElem,
+      lineBreak,
+      divEachItem,
+      imgCross,
+      imgCircle,
+      spanContent,
+      newContent
+    ) {
+      divTask.appendChild(divElem);
+      divTask.appendChild(lineBreak);
+      divElem.appendChild(divEachItem);
+      divElem.appendChild(imgCross);
+      divEachItem.appendChild(imgCircle);
+      divEachItem.appendChild(spanContent);
+      spanContent.appendChild(newContent);
+    }
+    //Add class for new elements
+    function setClass(divElem, divEachItem, imgCircle, imgCross, lineBreak) {
+      divElem.classList.add("elements");
+      divElem.classList.add("all");
+      divEachItem.classList.add("each-item");
+      imgCircle.classList.add("circle");
+      imgCross.classList.add("cross");
+      lineBreak.classList.add("horizontalLine");
+      lineBreak.classList.add("all");
+    }
+
+    // Left Items
+    function calcLeftItems() {
+      const leftItems =
+        document.getElementsByClassName("elements").length -
+        document.getElementsByClassName("completed").length / 2;
+      document.getElementById("left").innerHTML =
+        leftItems <= 1 ? leftItems + " item left" : leftItems + " items left";
     }
   }
-  // Left Items Function
-  function activeLen() {
-    const activeLen =
-      document.getElementsByClassName("elements").length -
-      document.getElementsByClassName("completed").length / 2;
-    if (activeLen <= 1) {
-      document.getElementById("left").innerHTML = activeLen + " item left";
-    } else {
-      document.getElementById("left").innerHTML = activeLen + " items left";
-    }
-  }
-}
-//Enter-key Checker Function
+} //End of createTodo()
+
+//Enter-key Checker
 function keyPressCheck(event) {
   if (event.which === 13) {
-    createElement();
+    createTodo();
   }
 }
-//Change theme Function
+//Change theme
 function changeTheme() {
-  debugger;
   isDark = !isDark;
   themeIcon.src = isDark ? "./images/sun.svg" : "./images/moon.svg";
 }
-// Show Completed Function
+// Show Completed
 function showCompleted() {
   const allObjects = document.getElementsByClassName("all");
-  for (let elems of Object.keys(allObjects)) {
-    allObjects[elems].classList.add("invisible");
+  for (let items of Object.keys(allObjects)) {
+    allObjects[items].classList.add("invisible");
   }
   const completedList = document.getElementsByClassName("completed");
-  for (let elems of Object.keys(completedList)) {
-    completedList[elems].classList.remove("invisible");
+  for (let items of Object.keys(completedList)) {
+    completedList[items].classList.remove("invisible");
   }
 }
-// Show All Function
+// Show All
 function showAll() {
-  const invisibleElems = document.getElementsByClassName("all");
-  for (let elems of Object.keys(invisibleElems)) {
-    invisibleElems[elems].classList.remove("invisible");
+  const invisibleElements = document.getElementsByClassName("all");
+  for (let items of Object.keys(invisibleElements)) {
+    invisibleElements[items].classList.remove("invisible");
   }
 }
-// Show Active Tasks Function
+// Show Active Tasks
 function showActive() {
   const allObjects = document.getElementsByClassName("all");
-  for (let elems of Object.keys(allObjects)) {
-    allObjects[elems].classList.remove("invisible");
+  for (let items of Object.keys(allObjects)) {
+    allObjects[items].classList.remove("invisible");
   }
   const completedList = document.getElementsByClassName("completed");
-  for (let elems of Object.keys(completedList)) {
-    completedList[elems].classList.add("invisible");
+  for (let items of Object.keys(completedList)) {
+    completedList[items].classList.add("invisible");
   }
 }
 
-//clear completed Functon
+//clear completed
 function clearCompleted() {
-  const completedObject = document.getElementsByClassName("completed");
-  while (completedObject.length > 0) {
-    completedObject[0].remove();
-  }
+  const completedObjects = document.getElementsByClassName("completed");
+  Object.keys(completedObjects).forEach((key) => completedObjects[0].remove());
 }
